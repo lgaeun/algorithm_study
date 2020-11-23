3f92ce14f3d3189bcaf69473310be9c590d65bbb
lgaeun <gaeungaeun4837@gmail.com>
2020-11-23 15:35:45 +0900

    1946 신입사원 이가은

--- 3주차/가은_1946.java
+++ 3주차/가은_1946.java
@@ -7,22 +7,22 @@ public class _1946 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int Ntest = Integer.parseInt(br.readLine());
 		int Nppl;
-		int[][] score = new int [100000][2];
+		int[][] rank = new int [100000][2];
 		StringTokenizer st;
 		
 		for(int i = 0; i < Ntest; i++) {
 			int count;
 			Nppl = Integer.parseInt(br.readLine());
 			
-			// 성적 저장 
+			// 등수 저장 
 			for(int j = 0; j < Nppl; j++) {
 				st = new StringTokenizer(br.readLine()," ");
-				score[j][0] = Integer.parseInt(st.nextToken());
-				score[j][1] = Integer.parseInt(st.nextToken());
+				rank[j][0] = Integer.parseInt(st.nextToken());
+				rank[j][1] = Integer.parseInt(st.nextToken());
 			}
 
 			//서류순으로 정렬
-			Arrays.sort(score,0,Nppl, new Comparator<int[]>() {
+			Arrays.sort(rank,0,Nppl, new Comparator<int[]>() {
 				
 				@Override
 				public int compare(int[] o1, int[] o2) { 
@@ -31,13 +31,13 @@ public class _1946 {
 			}
 			);
 			
-			// 면접성적 비교
-			int worst = score[0][1]; // 서류1등의 면접점수  
-			count = 1; //서류 1등 
+			// 면접등수 비교
+			int worst = rank[0][1]; // 서류1등의 면접등수  
+			count = 1; //서류 1등은 자동 포함
 			for(int j = 0; j < Nppl; j++) {
 				
-				if( worst > score[j][1]) {
-					worst = score[j][1];
+				if( worst > rank[j][1]) {
+					worst = rank[j][1];
 					count++;
 				}
 			}
