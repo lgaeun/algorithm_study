84af05cedac5c49db74074545fabff1e80e6a4d3
lgaeun <gaeungaeun4837@gmail.com>
2021-01-07 16:05:54 +0900

    2579 계단 오르기 이가은

---
+++ 8주차/2579_이가은.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[301];
+		int dp[] = new int[301];
+		
+		for(int i = 1; i <= n; i++) 
+			arr[i] = Integer.parseInt(br.readLine());
+		
+		//초기값 
+		dp[1] = arr[1];
+		dp[2] = arr[2]+dp[1];		
+		
+		for(int i = 3; i <= n; i++) 
+			dp[i] = max(dp[i-3] + arr[i] + arr[i-1], dp[i-2] + arr[i]);
+		
+		System.out.print(dp[n]);	
+	}
+	
+	static int max(int a, int b) {
+		return a > b ? a : b;
+	}
+    
+}
