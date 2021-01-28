b52530378972ec05efbfa63b07d91b2663cff168
lcheun <71924746+lcheun@users.noreply.github.com>
2021-01-28 18:28:48 +0900

    11051 이항계수2 이채은

---
+++ 11주차/11051_이채은.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[][] dp = new int[N+1][N+1];
+		for(int i=0; i<N+1; i++) {
+			for(int j=0; j<=i; j++) {
+				if (i == j || j == 0)
+					dp[i][j] = 1;
+				else
+					dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007;
+			}
+		}
+		System.out.println(dp[N][K]);
+		sc.close();
+	}
+}
