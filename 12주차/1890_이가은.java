8027e99faf7ac341d11197c96740ed37b9f755c0
lgaeun <55012143+lgaeun@users.noreply.github.com>
2021-02-04 17:32:40 +0900

    1890 점프 이가은 (주석추가)

--- 12주차/1890_이가은.java
+++ 12주차/1890_이가은.java
@@ -1,3 +1,6 @@
+/* 다이나믹프로그래밍 
+* 경로의 개수 263-1보다 작거나 같으므로 long
+*/
 import java.util.*;
 import java.io.*;
 
@@ -7,7 +10,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int [][]map = new int[n][n];
-		long [][]dp = new long[n][n];
+		long [][]dp = new long[n][n]; //경로의 개수
 		
 		StringTokenizer st; //입력 
 		for(int i = 0; i < n ; i++) {
@@ -16,13 +19,12 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		dp[0][0] = 1;
+		dp[0][0] = 1; //초기화
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				if(i==n-1 && j==n-1) break;
-				if(dp[i][j] != 0) {
+				if(i==n-1 && j==n-1) break; //종착점은 항상 0이기때문에, break하지 않으면 자기자신이 두 번 더 더해진다
+				if(dp[i][j] != 0) { ////0이 아닌 곳, 즉 길이 되는 곳만 계산
 					int step = map[i][j];
 					if(i+step < n) dp[i+step][j] += dp[i][j];
 					if(j+step < n) dp[i][j+step] += dp[i][j];
@@ -32,5 +34,4 @@ public class Main {
 		}
 		System.out.print(dp[n-1][n-1]);	
 	}
-
 }
