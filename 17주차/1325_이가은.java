1f5ede39bf16cba394d574d08d33afa3e5f243b4
lgaeun <55012143+lgaeun@users.noreply.github.com>
2021-03-11 18:24:34 +0900

    1325 효율적인 해킹 이가은

--- 17주차/1325_이가은.java
+++ 17주차/1325_이가은.java
@@ -22,22 +22,20 @@ public class Main {
 			input = br.readLine().split(" ");
 			int a = Integer.parseInt(input[0]);
 			int b = Integer.parseInt(input[1]);
-			list[a].add(b); // b해킹 -> a도 같이 해킹됌 
-      //list[b].add(a);
+			list[a].add(b); // 해킹되는 컴퓨터의 list (b해킹 -> a도 같이 해킹됌)
+     			 //list[b].add(a); -> (처음 풀이 방식) 해킹하는 컴퓨터의 list, bfs -> 시간초과
 		}
 		
 		for(int i = 1; i <= n; i++) {
 			visit = new boolean[n+1];
-			dfs(i);
+			dfs(i);	//각 컴퓨터를 해킹시킬 수 있는 컴퓨터 찾아서 번호 증가 
 		}
 		
 		int max = 0;
-		for (int i = 1; i <= n; i++) {
-			max = Math.max(max, hack[i]);
-		}
+		for (int i = 1; i <= n; i++) max = Math.max(max, hack[i]); //최대로 해킹할 수 있는 컴퓨터의 max 구하기
 		
-		for(int i = 1; i <= n; i++) 
-			if(hack[i] == max) sb.append(i+" ");
+		for(int i = 1; i <= n; i++) if(hack[i] == max) sb.append(i+" "); //max값과 같은 컴퓨터 번호 출력 
+		
 		System.out.println(sb.toString());
 	}
 	
@@ -45,7 +43,7 @@ public class Main {
 		visit[start] = true;
 		for(int v: list[start]) {
 			if(!visit[v]) {
-				hack[v]++; 
+				hack[v]++; //v를 해킹시키는 컴퓨터의 번호 증가시킴 
 				dfs(v);
 			}
 		}
