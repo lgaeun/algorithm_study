e76d694b2780e52ae12a310e79856865c75dcaec
lgaeun <55012143+lgaeun@users.noreply.github.com>
2021-05-19 18:05:39 +0900

    11048 이동하기 이가은

---
+++ 26주차/11048_이가은.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[n+1][m+1];
+		
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= m; j++) {
+				map[i][j] += Math.max(map[i-1][j-1], Math.max(map[i-1][j], map[i][j-1]));
+			}
+		}
+		System.out.println(map[n][m]);
+
+	}
+
+}
