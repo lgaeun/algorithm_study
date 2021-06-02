d7945d5104a0b4a77b7e3500ef02bee835324920
Haneul99 <55133691+Haneul99@users.noreply.github.com>
2021-06-02 20:18:30 +0900

    9081 단어 맞추기 정하늘 next_permutation

--- 28주차/9081_정하늘.cpp
+++ 28주차/9081_정하늘.cpp
@@ -7,50 +7,20 @@
 using namespace std;
 
 
-string str;
-string ans_str = "";
-
-
-int findSmaller(int idx) {
-	int smaller = 101;
-	for (int i = idx - 1; i >= 0; i--) {
-		if (str[i] < str[idx]) {
-			return i;
-		}
-	}
-	return smaller;
-}
-
-
 int main() {
 	int t;
 	scanf("%d", &t);
 	while (t--) {
-		printf("\n");
+		string str;
 		cin >> str;
-		int flag = 0;
-		for (int i = 0; i < str.size() - 1; i++) {
-			if (str[i] < str[i + 1]) {
-				flag = 1;
-				break;
-			}//내림차순 정렬이 아님
-		}
-
+		string ans = str;
+		int flag = next_permutation(str.begin(), str.end());
 		if (flag) {
-			for (int idx = str.size() - 1; idx > 0; idx--) {
-				int smaller = findSmaller(idx);
-				if (smaller == 101) continue;//찾는 값보다 작은 값이 없는 경우
-				else {
-					swap(str[idx], str[smaller]);
-					cout << "swap 후\n" << str << '\n';
-					sort(str.begin() + smaller + 1, str.end());
-					break;
-				}//switch 후, 뒤는 정렬
-				
-			}
-			cout << "정답\n" << str << '\n';
+			cout << str << '\n';
 		}
-		else cout << str << '\n';
+		else {
+			cout << ans << '\n';
+		}
 	}
 }
 
@@ -59,8 +29,19 @@ int main() {
 17070. 
 https://www.acmicpc.net/problem/17070
 
-1
-AZZZZBYYYYXXXXXXXXXXWWWWWWWWWWTTTTTTTTTTLLLLLLLLLLIIIIIIIIIIGGGGGGGGGGEEEEEEEEEECCCCCCCCCCBBBBBBBBBB
+
+
+
+
+5 6 7 8 9
+5 6 7 9 8
+5 6 8 9 7 -> 5 6 8 7 9
+5 6 8 9 7
+
+5 7 8 9 6 -> 5 7 6 8 9        ( 5 6 9 7 8 )
+
+
+
 
 
 */
